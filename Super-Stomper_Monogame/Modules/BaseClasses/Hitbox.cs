0588b6d68f38e590fd82cb0d32f3e0b6257e41ff
Najib <almousa.najib@gmail.com>
2023-12-18 22:22:38 +0100

    Create Class Hitbox

---
+++ Super-Stomper_Monogame/Modules/BaseClasses/Hitbox.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Super_Stomper_Monogame.Modules.BaseClasses
+{
+    internal class Hitbox
+    {
+        public Rectangle rectangle;
+        public Vector2 origin;
+
+        private Rectangle originalRect;
+        public Hitbox(Rectangle limit, Vector2 origin)
+        {
+            this.origin = origin;
+            rectangle = limit;
+            originalRect = rectangle;
+        }
+
+        public void Update(Vector2 position)
+        {
+            //hier moet ik updaten wanneer de hero move moet de hitbox meebewgen
+        }
+
+
+        
+         public bool IsTouching(Hitbox hitbox)
+        {
+            return rectangle.Intersects(hitbox.rectangle);
+        }
+    }
+}
--- Super-Stomper_Monogame/obj/Debug/net6.0-windows/Super_Stomper_Monogame.AssemblyInfo.cs
+++ Super-Stomper_Monogame/obj/Debug/net6.0-windows/Super_Stomper_Monogame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Super_Stomper_Monogame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d26846e59cd261aa82292ebd3d5d321cca4d9e47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Super_Stomper_Monogame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Super_Stomper_Monogame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
