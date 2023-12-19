fcb3e9bc4a6f306a1ada34da84deb2a6b68f7258
Najib <almousa.najib@gmail.com>
2023-12-19 13:45:07 +0100

    Implement collision detection on X direction

--- Super-Stomper_Monogame/Modules/BaseClasses/Physics.cs
+++ Super-Stomper_Monogame/Modules/BaseClasses/Physics.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+-
 
 
 namespace Super_Stomper_Monogame.Modules.BaseClasses
@@ -32,7 +33,7 @@ namespace Super_Stomper_Monogame.Modules.BaseClasses
 
         public void Update(float deltaTime)
         {
-            
+
             desiredVelocity.X -= desiredVelocity.X * dragScale;
             //desiredVelocity.Y += gravityConstant * (desiredVelocity.Y >= 0 ? fallingGravityScale : risingGravityScale);
             desiredVelocity.X = desiredVelocity.X >= 0 ? (float)System.Math.Floor(desiredVelocity.X) : (float)System.Math.Ceiling(desiredVelocity.X);
@@ -52,9 +53,26 @@ namespace Super_Stomper_Monogame.Modules.BaseClasses
                 movement.deltaY < 0 ? (int)System.Math.Floor(movement.deltaY) : (int)System.Math.Ceiling(movement.deltaY)
             );
             Rectangle offsetedRectangle = myHitbox.rectangle;
+
+            if (myHitbox.IsTouching(otherHitbox))
+            {
+                myHitbox.rectangle = originalRect;
+
+                Ray ray1 = new Ray(new Vector3(new Vector2(movement.deltaX > 0 ? myHitbox.rectangle.Right : myHitbox.rectangle.Left, myHitbox.rectangle.Top), 0), new Vector3(movement.deltaX, 0, 0));
+                Ray ray2 = new Ray(new Vector3(new Vector2(movement.deltaX > 0 ? myHitbox.rectangle.Right : myHitbox.rectangle.Left, myHitbox.rectangle.Bottom), 0), new Vector3(movement.deltaX, 0, 0));
+
+
+                BoundingBox boundingBoxH = movement.deltaY > 0 ? GetTopBoundingBox(otherHitbox.rectangle) : GetBottomBoundingBox(otherHitbox.rectangle);
+
+                
+            }
             return collisionType;
-           
         }
+        private BoundingBox GetTopBoundingBox(Rectangle rect) => new BoundingBox(new Vector3(rect.Left, rect.Top, 0), new Vector3(rect.Right, rect.Top, 0));
+        private BoundingBox GetBottomBoundingBox(Rectangle rect) => new BoundingBox(new Vector3(rect.Left, rect.Bottom, 0), new Vector3(rect.Right, rect.Bottom, 0)
+
+
 
     }
+
 }
