42789b2bebbc439b29c67bca0f665e10f1f64e6e
Najib <almousa.najib@gmail.com>
2023-12-25 22:22:08 +0100

    Add animation to class Coins

--- Super-Stomper_Monogame/Modules/Game/Coins.cs
+++ Super-Stomper_Monogame/Modules/Game/Coins.cs
@@ -8,6 +8,7 @@ namespace Super_Stomper_Monogame.Modules.Game
     internal class Coins
     {
         private Sprite sprite;
+        private Animation animation;
 
         private const int coinWidth = 16;
         private const int coinHeight = 16;
@@ -15,17 +16,17 @@ namespace Super_Stomper_Monogame.Modules.Game
         public Coins(ContentManager content, Vector2 position)
         {
             sprite = new Sprite(content.Load<Texture2D>(@"Spritesheets\Coins\coin"), new Rectangle(0, coinHeight, coinWidth, coinHeight), Vector2.Zero, position);
-
+            animation = new Animation(sprite.texture, 0.15f, true, new int[] { 0 }, sprite.sourceRect.Size);
         }
 
         public void Update(float deltaTime)
         {
-
+            animation.Update(deltaTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            sprite.sourceRect = animation.GetCurrentFrame();
             sprite.Draw(spriteBatch, sprite.position);
         }
     }
