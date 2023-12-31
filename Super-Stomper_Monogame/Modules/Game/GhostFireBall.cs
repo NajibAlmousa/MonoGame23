0cf46827ef9f17f45ef3dfca4751a727aacbe849
Najib <almousa.najib@gmail.com>
2023-12-31 15:33:04 +0100

    Update class GhostFireBall

--- Super-Stomper_Monogame/Modules/Game/GhostFireBall.cs
+++ Super-Stomper_Monogame/Modules/Game/GhostFireBall.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Content;
+using Super_Stomper_Monogame.Modules.BaseClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 namespace Super_Stomper_Monogame.Modules.Game
 {
     internal class GhostFireBall
     {
+        private Sprite sprite;
+        private Animation animation;
+
+        private const int ballSize = 32;
+
+
+        public GhostFireBall(ContentManager content, Vector2 position)
+        {
+            sprite = new Sprite(content.Load<Texture2D>(@"Spritesheet/Enemies\ghost-FireBall"), new Rectangle(17, 0, ballSize, ballSize), Vector2.Zero);
+
+            animation = new Animation(sprite.texture, 0.2f, true, new int[] { 2, 3, 4, 5 }, new Point(ballSize));
+        }
+
+
+        public void Update(float deltaTime)
+        {
+            animation.Update(deltaTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.sourceRect = animation.GetCurrentFrame();
+        }
+
     }
 }
