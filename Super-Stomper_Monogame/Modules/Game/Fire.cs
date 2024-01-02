2f6fc57d69ec3fef70f0cd93e1e17db093cec4be
Najib <almousa.najib@gmail.com>
2024-01-02 14:05:01 +0100

    Update class Fire with Function

--- Super-Stomper_Monogame/Modules/Game/Fire.cs
+++ Super-Stomper_Monogame/Modules/Game/Fire.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using Super_Stomper_Monogame.Modules.BaseClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Super_Stomper_Monogame.Modules.Game
 {
-    internal class Fire
+    internal class Fire : IEnemy
     {
+        private Sprite sprite;
+        private Animation animation;
+        private Movement movement;
+
+        private const int fireWidth = 32;
+        private const int fireHeight = 32;
+        public Fire(ContentManager content, Vector2 position)
+        {
+            sprite = new Sprite(content.Load<Texture2D>(@"Spritesheets\Enemies\Fire_2"), new Rectangle(7 * fireWidth, 0, fireWidth, fireHeight), Vector2.Zero, position);
+            animation = new Animation(sprite.texture, 0.25f, true, new int[] { 0,1,2,3,4 }, sprite.sourceRect.Size);
+            movement = new Movement(position);
+        }
+
+
+        public void Update(float deltaTime, Vector2 myHeroPosition)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.sourceRect = animation.GetCurrentFrame();
+            sprite.Draw(spriteBatch, movement.position);
+        }
     }
 }
